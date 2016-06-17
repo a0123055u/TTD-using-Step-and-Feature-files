6b9cb65b22b32272d6c11853a285d6b6ccc1d992
a0123055u <festecole06>
2016-06-17 14:32:01 +0800

    Refactored for jenkins

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 
 
 @RunWith(Cucumber.class)
-//@CucumberOptions(features="src/attd.feature")
+@CucumberOptions(features="src/test/java/attd.feature")
 public class TestRunner {
 	
 	
--- src/test/java/TestRunnerExport.java
+++ src/test/java/TestRunnerExport.java
@@ -2,10 +2,12 @@ package src.test.java;
 
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 
 @RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/java/Export.feature")
 public class TestRunnerExport {
 	
 	 
